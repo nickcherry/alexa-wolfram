4a82a178f643ac70c372c00ef4c5495a1f2bf763
Nick Cherry <ncherry@cipherhealth.com>
2016-08-27 12:39:04 -0400

    resolve paths during build

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-TARGET_FILE="build.zip"
+SCRIPT_PATH=`realpath $0`
+ROOT=`dirname $SCRIPT_PATH`
+
+APP_DIR="$ROOT/apps/wolfram/"
+TARGET_FILE="$ROOT/build.zip"
 WHITELISTED_FILES="*.js *.json node_modules"
 
 rm $TARGET_FILE
+cd $APP_DIR
 zip -r $TARGET_FILE $WHITELISTED_FILES
