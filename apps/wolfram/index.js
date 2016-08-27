c582db5baa6ad9eaa8153410305d18e8562456de
Nick Cherry <ncherry@cipherhealth.com>
2016-08-27 16:31:12 -0400

    add bugsnag

--- apps/wolfram/index.js
+++ apps/wolfram/index.js
@@ -13,6 +13,7 @@ module.change_code = 1;
 
 const alexa = require('alexa-app');
 const app = new alexa.app('wolfram');
+const bugsnag = require("bugsnag");
 
 const Config = require('./config');
 const WolframService = require('./wolfram_service');
@@ -24,25 +25,28 @@ const WolframService = require('./wolfram_service');
 
 const wolfram = new WolframService(Config.wolframAppId);
 
-app.intent('askWolfram',
-  {
-    slots: {
-      query: 'LITERAL'
+bugsnag.register(Config.bugsnagApiKey);
+bugsnag.autoNotify({ context: 'askWolfram' }, () => {
+  app.intent('askWolfram',
+    {
+      slots: {
+        query: 'LITERAL'
+      }
+    }, (req, res) => {
+      const query = req.slot('query');
+      const handleError = (err) => {
+        res.say("Well, shit. Something went wrong.").send()
+      };
+      wolfram.ask(query)
+        .then((wolframResponse) => {
+          const answer = wolfram.extractResultText(wolframResponse.body);
+          answer ? res.say(answer).send() : handleError();
+        })
+        .catch(handleError);
+      return false; // returning false tells alexa-app this is an async request
     }
-  }, (req, res) => {
-    const query = req.slot('query');
-    const handleError = (err) => {
-      res.say("Well, shit. Something went wrong.").send()
-    };
-    wolfram.ask(query)
-      .then((wolframResponse) => {
-        const answer = wolfram.extractResultText(wolframResponse.body);
-        answer ? res.say(answer).send() : handleError();
-      })
-      .catch(handleError);
-    return false; // returning false tells alexa-app this is an async request
-  }
-);
+  );
+});
 
 /***********************************************/
 /* Exports */
